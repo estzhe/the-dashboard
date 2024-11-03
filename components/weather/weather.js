4f6f175e345542dcf223c8ce80104e93f82e6670
Kamal Eybov <64245977+estzhe@users.noreply.github.com>
2024-11-03 11:39:15 -0800

    Weather component: move to Open Meteo and some improvements.

--- components/weather/weather.js
+++ components/weather/weather.js
@@ -29,18 +29,7 @@ export default class WeatherComponent extends BaseComponent
         Argument.notNullOrUndefined(container, "container");
         Argument.notNullOrUndefined(data, "data");
 
-        for (const h of data.hourly)
-        {
-            const dateTime = Temporal.Instant.fromEpochSeconds(h.dt).toZonedDateTimeISO(Temporal.Now.timeZoneId());
-            h.displayTime = dateTime.hour % 2 === 0
-                ? dateTime
-                    .toLocaleString(undefined /* current locale */, { hour: "numeric", hourCycle: "h12" })
-                    .replace(" ", "")
-                    .toLowerCase()
-                : "";
-        }
-
-        container.innerHTML = await this._template("template", data);
+        container.innerHTML = await this._template("template", data.forecast);
 
         const elements = {
             temperatureCanvas: container.querySelector(".temperature"),
@@ -54,20 +43,20 @@ export default class WeatherComponent extends BaseComponent
 
         WeatherGraphics.drawStrips(
             elements.temperatureCanvas,
-            data.hourly.map(h => h.temp),
+            data.forecast.map(h => h.temperature_2m),
             {
-                start: { value: 0, color: "#000000" },
-                end: { value: 30, color: "#ffff00" },
+                start: { value: data.stats.minTemperature, color: "#000000" },
+                end: { value: data.stats.maxTemperature, color: "#ffff00" },
             },
             /* randomnessFactor */ 1,
         );
 
         WeatherGraphics.drawStrips(
             elements.cloudsCanvas,
-            data.hourly.map(h => h.clouds),
+            data.forecast.map(h => h.cloud_cover),
             {
-                start: { value: 0, color: "rgb(183, 185, 182)" },
-                end: { value: 100, color: "rgb(94, 182, 255)" },
+                start: { value: 0, color: "rgb(94, 182, 255)" },
+                end: { value: 100, color: "rgb(183, 185, 182)" },
             },
             /* randomnessFactor */ 1,
         );
@@ -76,12 +65,12 @@ export default class WeatherComponent extends BaseComponent
             elements.rainAndSnowCanvas,
             [
                 {
-                    values: data.hourly.map(h => h.snow?.["1h"] ?? 0),
+                    values: data.forecast.map(h => h.snowfall),
                     color: "white",
                     scale: { min: 0, max: 1 },
                 },
                 {
-                    values: data.hourly.map(h => h.rain?.["1h"] ?? 0),
+                    values: data.forecast.map(h => h.rain + h.showers),
                     color: "#1878f0",
                     scale: { min: 0, max: 2 },
                 },
@@ -95,23 +84,103 @@ export default class WeatherComponent extends BaseComponent
             "data",
             async () =>
             {
-                const apiKey = WeatherComponent.#getApiKey();
                 const location = await WeatherComponent.#getLocation();
-                return await WeatherComponent.#fetchWeatherData(location, apiKey);
+
+                const results = await Promise.all([
+                    WeatherComponent.#fetchForecast(location),
+                    WeatherComponent.#fetchTemperatureStats(location),
+                ]);
+
+                const forecast = WeatherComponent.#distillForecast(results[0]);
+                const stats = WeatherComponent.#distillStats(results[1]);
+                
+                return {
+                    forecast,
+                    stats,
+                };
             },
             refreshData);
     }
 
-    static async #fetchWeatherData(location, apiKey)
+    static #distillForecast(forecastData)
+    {
+        Argument.notNullOrUndefined(forecastData, "forecastData");
+
+        // There is no way to filter by exact date and time, so we get extra records
+        // that need to be filtered out.
+        const startDate = Temporal.Now.plainDateISO();
+        const endDate = startDate.add({ days: 2 }); // today + tomorrow + the day after tomorrow
+
+        return forecastData.hourly.time
+            .map((time, i) => {
+                // We request data in GMT, but Open-Meteo returns times without offset.
+                const timeStringWithOffset = time + "+00:00";
+                // Subtract an hour, because Open-Meteo returns data for preceeding hour,
+                // while we want to display it for the following hour.
+                const dateTime = Temporal.Instant.from(timeStringWithOffset)
+                    .subtract({ hours: 1 })
+                    .toZonedDateTimeISO(Temporal.Now.timeZoneId());
+                
+                if (Temporal.PlainDate.compare(dateTime, startDate) < 0 ||
+                    Temporal.PlainDate.compare(dateTime, endDate) > 0)
+                {
+                    return null;
+                }
+
+                const displayTime = dateTime.hour % 2 === 0
+                    ? dateTime
+                        .toLocaleString(undefined /* current locale */, { hour: "numeric", hourCycle: "h23" })
+                        .replace(" ", "")
+                        .toLowerCase()
+                    : "";
+
+                return {
+                    dateTime,
+                    displayTime,
+                    temperature_2m: forecastData.hourly.temperature_2m[i],
+                    rain: forecastData.hourly.rain[i],
+                    showers: forecastData.hourly.showers[i],
+                    snowfall: forecastData.hourly.snowfall[i],
+                    cloud_cover: forecastData.hourly.cloud_cover[i],
+                };
+            })
+            .filter(e => e !== null);
+    }
+
+    static #distillStats(statsData)
+    {
+        Argument.notNullOrUndefined(statsData, "statsData");
+
+        return {
+            minTemperature: statsData.daily.temperature_2m_min.reduce((p, c) => Math.min(p, c)),
+            maxTemperature: statsData.daily.temperature_2m_max.reduce((p, c) => Math.max(p, c)),
+        };
+    }
+
+    static async #fetchForecast(location)
     {
         Argument.notNullOrUndefined(location, "location");
-        Argument.notNullOrUndefinedOrEmpty(apiKey, "apiKey");
 
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/onecall` +
-                                        `?lat=${location.latitude}` +
-                                        `&lon=${location.longitude}` +
-                                        `&appid=${apiKey}` +
-                                        `&units=metric`);
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast` +
+            `?latitude=${location.latitude}` +
+            `&longitude=${location.longitude}` +
+            `&hourly=temperature_2m,rain,showers,snowfall,cloud_cover` +
+            `&forecast_days=4`);
+
+        return await response.json();
+    }
+
+    static async #fetchTemperatureStats(location)
+    {
+        Argument.notNullOrUndefined(location, "location");
+
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast` +
+            `?latitude=${location.latitude}` +
+            `&longitude=${location.longitude}` +
+            `&daily=temperature_2m_max,temperature_2m_min` +
+            `&past_days=14` +
+            `&forecast_days=4`);
+
         return await response.json();
     }
 
@@ -123,24 +192,5 @@ export default class WeatherComponent extends BaseComponent
                 position => resolve(position.coords),
                 () => reject("Count not get location."));
         });
-    }
-
-    static #getApiKey()
-    {
-        const key = `openweathermap.apikey`;
-
-        let apiKey = localStorage.getItem(key);
-        if (!apiKey)
-        {
-            apiKey = prompt(`Please enter API key for OpenWeatherMap.`);
-            if (!apiKey)
-            {
-                throw new Error("An OpenWeatherMap API key was not provided.");
-            }
-
-            localStorage.setItem(key, apiKey);
-        }
-
-        return apiKey;
     }
 }
